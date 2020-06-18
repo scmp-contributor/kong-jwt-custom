eeb1a24ec4c08fe7802e509f590d3a1455aaf56e
Quintus Leung <quintus.leung@scmp.com>
2020-06-18 17:04:11 +0800

    Fix schema name

--- schema.lua
+++ schema.lua
@@ -2,7 +2,7 @@ local typedefs = require "kong.db.schema.typedefs"
 
 
 return {
-  name = "jwt",
+  name = "jwt-custom",
   fields = {
     { protocols = typedefs.protocols_http },
     { config = {
